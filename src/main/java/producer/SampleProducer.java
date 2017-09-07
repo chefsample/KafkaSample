0bd5e03daaa586f3ab8163be3825879bdfeea3e4
Kumar <kumar0616@hotmail.com>
2017-09-06 23:50:35 -0700

    Changes to POM. Tried something new

--- src/main/java/producer/SampleProducer.java
+++ src/main/java/producer/SampleProducer.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import java.util.Properties;
 
 public class SampleProducer {
+
     public static void main(String args[]){
 
         Properties props = new Properties();
